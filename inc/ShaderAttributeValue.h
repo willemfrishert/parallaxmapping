351719d49ea7d790a40715ec72a63a8e651ff43b
jpjorge <jpjorge@18efd892-9420-0410-97b9-638391ccb344>
2006-10-25 21:30:27 +0000

    Shader Objects

---
+++ inc/ShaderAttributeValue.h
@@ -0,0 +1,63 @@
+/**
+ * @description Represents a uniform shader attribute
+ * TODO: support for matrices
+ * 
+ * @file ShaderAttributeValue.h
+ * @author João Pedro Jorge
+ */
+#pragma once
+
+#include "ShaderAttributeObject.h"
+
+template <class T>
+class ShaderAttributeValue: public ShaderAttributeObject
+{
+public:
+	ShaderAttributeValue(void);
+	virtual ~ShaderAttributeValue(void);
+
+	void setValue(char* name, T value);
+	
+	virtual void use() const;
+
+	// attributes
+private:
+	T value;
+};
+
+template <class T>
+ShaderAttributeValue<T>::ShaderAttributeValue(void)
+{
+	this->name = NULL;
+	this->location = -1;
+}
+
+template <class T>
+ShaderAttributeValue<T>::~ShaderAttributeValue(void)
+{
+	free( this->name );
+}
+
+/**
+* @param name the uniform attribute name
+* @param value the attribute value
+*/
+template <class T>
+void ShaderAttributeValue<T>::setValue(char* name, T value)
+{
+	this->value = value;
+	size_t len = strlen(name) + 1;
+	this->name = (char*) malloc(sizeof(char) * len);
+#ifdef WIN32
+	strcpy_s(this->name, len, name);
+#else
+	strcpy(this->name, len, name);
+#endif
+}
+
+template <class T>
+void ShaderAttributeValue<T>::use() const
+{
+	setUniform(this->location, this->value);
+}
+
---
+++ inc/ShaderAttributeVector.h
@@ -0,0 +1,75 @@
+/**
+* @description Represents a uniform shader attribute
+* TODO: support for matrices
+* 
+* @file ShaderAttributeVector.h
+* @author João Pedro Jorge
+*/
+#pragma once
+
+#include "ShaderAttributeObject.h"
+
+
+// specifies the type of uniform variable
+enum VECSIZE {VEC3 = 3, VEC4 = 4};
+
+template <class T>
+class ShaderAttributeVector: public ShaderAttributeObject
+{
+public:
+	ShaderAttributeVector(VECSIZE size);
+	virtual ~ShaderAttributeVector(void);
+
+	void setValues(char* name, T* values);
+
+	virtual void use() const;
+
+	// attributes
+private:
+	T* values;
+	VECSIZE size;
+};
+
+template <class T>
+ShaderAttributeVector<T>::ShaderAttributeVector(VECSIZE size)
+{
+	this->name = NULL;
+	this->location = -1;
+	this->size = size;
+	values = new T[ size ];
+}
+
+template <class T>
+ShaderAttributeVector<T>::~ShaderAttributeVector(void)
+{
+	free( this->name );
+	delete values;
+}
+
+/**
+* @param name the uniform attribute name
+* @param value the attribute value
+*/
+template <class T>
+void ShaderAttributeVector<T>::setValues(char* name, T* values)
+{
+	size_t len = strlen(name) + 1;
+	this->name = (char*) malloc(sizeof(char) * len);
+#ifdef WIN32
+	strcpy_s(this->name, len, name);
+#else
+	strcpy(this->name, len, name);
+#endif
+
+	for (int i = 0; i < this->size; i++)
+	{
+		this->values[ i ] = values[ i ];
+	}
+}
+
+template <class T>
+void ShaderAttributeVector<T>::use() const
+{
+	setUniform(this->location, this->values, this->size);
+}
+
