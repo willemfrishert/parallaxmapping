af15634a86d809e9777ad94a69f38a8c7ce6ded2
jpjorge <jpjorge@18efd892-9420-0410-97b9-638391ccb344>
2006-10-25 21:38:22 +0000

    indented stuff

--- trunk/src/CMesh.cpp
+++ trunk/src/CMesh.cpp
@@ -1,5 +1,7 @@
 #include "CMesh.h"
 
+#define ROUNDOFF(val) val
+
 CMesh::CMesh()
 : iIndicesCount( 0 )
 {
@@ -93,54 +95,88 @@ void CMesh::draw()
 #endif
 }
 
-void CMesh::CreateInverseTBNMatrix(const TVector3<float> aVertices[3], const TVector2<float> aTextureCoordinates[3],
-                                   TVector3<float>& aInverseNormal, TVector3<float>& aInverseBinormal, TVector3<float>& aInverseTangent)
+/************************************************************************/
+/* CONVENIENCE CLASS TTRIANGLEITERATOR                                  */
+/************************************************************************/
+TTriangleIterator::TTriangleIterator(CMesh* aMesh)
+: iMesh( aMesh )
 {
-  TVector3<float> v0v1 = aVertices[1] - aVertices[0];
-  TVector3<float> v0v2 = aVertices[2] - aVertices[0];
-  
-  float c0c1_T = aTextureCoordinates[1].x() - aTextureCoordinates[0].x();
-  float c0c1_B = aTextureCoordinates[1].y() - aTextureCoordinates[0].y();
-  
-  float c0c2_T = aTextureCoordinates[2].x() - aTextureCoordinates[0].x();
-  float c0c2_B = aTextureCoordinates[2].y() - aTextureCoordinates[0].y();
-  
-  float denominator = c0c1_T*c0c2_B-c0c2_T*c0c1_B;
-  
-  if ( ROUNDOFF(denominator) )
-  {
-  aInverseTangent = TVector3<float>(1.0f, 0.0f, 0.0f);
-  aInverseBinormal= TVector3<float>(0.0f, 1.0f, 0.0f);
-  aInverseNormal  = TVector3<float>(0.0f, 0.0f, 1.0f);
-  }
-  else
-  {
-  float fScale1=1.0f/denominator;
-  
-  TVector3<float> T = TVector3<float>( (c0c2_B  * v0v1.x() - c0c1_B * v0v2.x()) * fScale1,
-                                       (c0c2_B  * v0v1.y() - c0c1_B * v0v2.y()) * fScale1,
-                                       (c0c2_B  * v0v1.z() - c0c1_B * v0v2.z()) * fScale1 );
-  
-  TVector3<float> B = TVector3<float>( (-c0c2_T * v0v1.x() + c0c1_T * v0v2.x()) * fScale1,
-                                       (-c0c2_T * v0v1.y() + c0c1_T * v0v2.y()) * fScale1,
-                                       (-c0c2_T * v0v1.z() + c0c1_T * v0v2.z()) * fScale1 );
-  
-  TVector3<float> N = T^B;
-  
-  float fScale2 = 1.0f / ( (T.x() * B.y() * N.z() - T.z() * B.y() * N.x()) +
-                           (B.x() * N.y() * T.z() - B.z() * N.y() * T.x()) +
-                           (N.x() * T.y() * B.z() - N.z() * T.y() * B.x()) );
-  
-  aInverseTangent.set(  (B^N).x() * fScale2,
-                        ((-N)^T).x() * fScale2,
-                        (T^B).x() * fScale2 );
-  
-  aInverseBinormal.set( ((-B)^N).y() * fScale2,
-                        (N^T).y() * fScale2,
-                        ((-T)^N).y() * fScale2 );
-  
-  aInverseNormal.set(   (B^N).z() * fScale2,
-                        ((-N)^T).z() * fScale2,
-                        (T^B).z() * fScale2 );
-  }
+	this->iCurrentTriangle[0] = 0;
+	this->iCurrentTriangle[1] = 1;
+	this->iCurrentTriangle[2] = 2;
+}
+
+bool TTriangleIterator::getNextTriangle(GLuint* aIndices)
+{
+	if (this->iCurrentTriangle[2] < static_cast<GLuint>( this->iMesh->getIndicesCount() ))
+	{
+		this->iMesh->getTriangleIndices(this->iCurrentTriangle, aIndices);
+
+		this->iCurrentTriangle[0]++;
+		this->iCurrentTriangle[1]++;
+		this->iCurrentTriangle[2]++;
+
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * @param aVertices
+ * @param aTextureCoordinates
+ * @param aInverseNormal
+ * @param aInverseBinormal
+ * @param aInverseTangent
+ */
+void CMesh::CreateInverseTBNMatrix(const TVector3<float> aVertices[3], const TVector2<float> aTextureCoordinates[3],
+								   TVector3<float>& aInverseNormal, TVector3<float>& aInverseBinormal, TVector3<float>& aInverseTangent)
+{
+	TVector3<float> v0v1 = aVertices[1] - aVertices[0];
+	TVector3<float> v0v2 = aVertices[2] - aVertices[0];
+
+	float c0c1_T = aTextureCoordinates[1].x() - aTextureCoordinates[0].x();
+	float c0c1_B = aTextureCoordinates[1].y() - aTextureCoordinates[0].y();
+
+	float c0c2_T = aTextureCoordinates[2].x() - aTextureCoordinates[0].x();
+	float c0c2_B = aTextureCoordinates[2].y() - aTextureCoordinates[0].y();
+
+	float denominator = c0c1_T*c0c2_B-c0c2_T*c0c1_B;
+
+	if ( ROUNDOFF(denominator) )
+	{
+		aInverseTangent = TVector3<float>(1.0f, 0.0f, 0.0f);
+		aInverseBinormal= TVector3<float>(0.0f, 1.0f, 0.0f);
+		aInverseNormal  = TVector3<float>(0.0f, 0.0f, 1.0f);
+	}
+	else
+	{
+		float fScale1=1.0f/denominator;
+
+		TVector3<float> T = TVector3<float>( (c0c2_B  * v0v1.x() - c0c1_B * v0v2.x()) * fScale1,
+			(c0c2_B  * v0v1.y() - c0c1_B * v0v2.y()) * fScale1,
+			(c0c2_B  * v0v1.z() - c0c1_B * v0v2.z()) * fScale1 );
+
+		TVector3<float> B = TVector3<float>( (-c0c2_T * v0v1.x() + c0c1_T * v0v2.x()) * fScale1,
+			(-c0c2_T * v0v1.y() + c0c1_T * v0v2.y()) * fScale1,
+			(-c0c2_T * v0v1.z() + c0c1_T * v0v2.z()) * fScale1 );
+
+		TVector3<float> N = T^B;
+
+		float fScale2 = 1.0f / ( (T.x() * B.y() * N.z() - T.z() * B.y() * N.x()) +
+			(B.x() * N.y() * T.z() - B.z() * N.y() * T.x()) +
+			(N.x() * T.y() * B.z() - N.z() * T.y() * B.x()) );
+
+		aInverseTangent.set(  (B^N).x() * fScale2,
+			((-N)^T).x() * fScale2,
+			(T^B).x() * fScale2 );
+
+		aInverseBinormal.set( ((-B)^N).y() * fScale2,
+			(N^T).y() * fScale2,
+			((-T)^N).y() * fScale2 );
+
+		aInverseNormal.set(   (B^N).z() * fScale2,
+			((-N)^T).z() * fScale2,
+			(T^B).z() * fScale2 );
+	}
 }
