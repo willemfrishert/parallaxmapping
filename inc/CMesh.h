0a85aa94a48ceb84193234c27aaf6a4a61b36583
jpjorge <jpjorge@18efd892-9420-0410-97b9-638391ccb344>
2006-10-25 21:37:42 +0000

    indented stuff

--- inc/CMesh.h
+++ inc/CMesh.h
@@ -3,6 +3,22 @@
 #include "Basic.h"
 #include "TVertex.h"
 
+class CMesh;
+
+/**
+ * @description Convenience class to iterate through the triangles
+ * of a mesh
+ */
+class TTriangleIterator
+{
+	TTriangleIterator(CMesh* aMesh);
+	bool getNextTriangle(GLuint* aIndices);
+
+private:
+	GLuint iCurrentTriangle[3];
+	CMesh* iMesh;
+};
+
 class CMesh
 {
   //PUBLIC METHODS
@@ -14,13 +30,17 @@ public:
 
 	void addVertex(const TVertex& aVertex, int aIndex = -1);
 	void addTriangleIndices(const int& aIndex1, const int& aIndex2, const int& aIndex3);
+	
 	TVertex& getVertex(int aIndex = -1);
+	int getIndicesCount() const;
+	void getTriangleIndices(const GLuint* aTriangleIndices, GLuint* aVertexIndices) const;
 
 	void setBinormalLocation(GLuint aLocation);
 	void setTangentLocation(GLuint aLocation);
-  //PRIVATE METHODS
+  
+	//PRIVATE METHODS
 private:
-  void CreateInverseTBNMatrix(const TVector3<float> aVertices[3], const TVector2<float> aTextureCoordinates[3],
+	void CreateInverseTBNMatrix(const TVector3<float> aVertices[3], const TVector2<float> aTextureCoordinates[3],
                               TVector3<float>& aInverseNormal, TVector3<float>& aInverseBinormal, TVector3<float>& aInverseTangent);
 
   //PUBLIC VARIABLES
@@ -66,6 +86,32 @@ TVertex& CMesh::getVertex(int aIndex)
 {
 	return this->iVertices.at( aIndex );
 }
+
+/**
+ * @return the number of triangle indices in the mesh
+ */
+inline
+int CMesh::getIndicesCount() const
+{
+	return this->iIndicesCount;
+}
+/**
+ * @param aTriangleIndices the indices that correspond to the triangle description: input
+ * @param aVertexIndices the indices of the vertices that describe the triangle: output
+ */
+inline
+void CMesh::getTriangleIndices(const GLuint* aTriangleIndices, GLuint* aVertexIndices) const
+{
+	GLuint count = static_cast<GLuint>( this->getIndicesCount() );
+	assert( (aTriangleIndices[0] < count) &&
+		(aTriangleIndices[1] < count) &&
+		(aTriangleIndices[2] < count));
+
+	aVertexIndices[ 0 ] = this->iIndices[ aTriangleIndices[0] ];
+	aVertexIndices[ 1 ] = this->iIndices[ aTriangleIndices[1] ];
+	aVertexIndices[ 2 ] = this->iIndices[ aTriangleIndices[2] ];
+}
+
 
 /**
  * @param aIndex1 the first index of the triangle
