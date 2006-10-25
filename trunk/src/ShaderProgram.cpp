3a75195116325ad6d0b72622789b87ebad90b920
jpjorge <jpjorge@18efd892-9420-0410-97b9-638391ccb344>
2006-10-25 21:30:58 +0000

    Shader Objects

---
+++ trunk/src/ShaderProgram.cpp
@@ -0,0 +1,78 @@
+#include <windows.h>
+#include <gl/glew.h>
+#include "ShaderObject.h"
+#include "ShaderAttributeObject.h"
+#include "ShaderProgram.h"
+#include "ExtensionsLoader.h"
+
+ShaderProgram::ShaderProgram(void)
+{
+	this->program = glCreateProgram();
+}
+
+ShaderProgram::~ShaderProgram(void)
+{
+}
+
+/**
+ * @param obj
+ */
+void ShaderProgram::attachShader(const ShaderObject& obj)
+{
+	glAttachShader( this->program, obj.getShaderId() );
+}
+
+void ShaderProgram::addUniformObject(ShaderAttributeObject* obj)
+{
+	//int status = obj->assignLocation( this->program );
+
+	//if (status != -1)
+	{
+		this->uniformObjects.push_back( obj );
+	}
+}
+
+/**
+ */
+void ShaderProgram::buildProgram()
+{
+	// Link the program object and print out the info log.
+	glLinkProgram( this->program );
+	glGetProgramiv( this->program, GL_LINK_STATUS, &this->shadersLinked );
+
+	if( this->shadersLinked == GL_FALSE )
+	{
+		char statusString[4096];
+		glGetProgramInfoLog( this->program, sizeof(statusString), NULL, statusString );
+		ExtensionsLoader::printError("Program object linking error", statusString);
+	}
+	else
+	{
+		list< ShaderAttributeObject* >::iterator it = this->uniformObjects.begin();
+		for (; it != this->uniformObjects.end(); it++)
+		{
+			(*it)->assignLocation( this->program );
+		}
+	}
+}
+
+/**
+ */
+void ShaderProgram::useProgram()
+{
+	glUseProgram( this->program );
+
+	list< ShaderAttributeObject* >::const_iterator uniformIter = this->uniformObjects.begin();
+
+	for (; uniformIter != this->uniformObjects.end() ; uniformIter++)
+	{
+		(*uniformIter)->use();
+	}
+}
+
+/**
+ */
+void ShaderProgram::disableProgram()
+{
+	glUseProgram( 0 );
+}
